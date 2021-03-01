a3979326c000c5c5a9d7c72d01886b8b78b38e18
Eric Chi <ericchi.311@gmail.com>
2021-03-01 06:26:41 -0500

    create a test to compile, deploy a contract to local test network

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,6 +1,23 @@
 const assert = require('assert');
-const ganache = require('ganache-cli'); // gives us access to a provider specific to the network
+const ganache = require('ganache-cli'); // gives us access to a provider specific to the network;
 const Web3 = require('web3'); // bringing in a constructor
-
 const web3 = new Web3(ganache.provider());
+const { bytecode, interface } = require('../compile');
 
+let accounts;
+
+beforeEach(async () => {
+    //get a list of all account
+    accounts = await web3.eth.getAccounts();
+
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there'] })
+        .send({ from: accounts[0], gas: '1000000'})
+    //use one of them to deploy the contract
+})
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        console.log(inbox);
+    })
+})

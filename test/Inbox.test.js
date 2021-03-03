742e876a6789c3f886b08f862d7a3e8e19a6c077
Eric Chi <ericchi.311@gmail.com>
2021-03-03 06:35:28 -0500

    write test checking for default message on contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,23 +1,32 @@
 const assert = require('assert');
 const ganache = require('ganache-cli'); // gives us access to a provider specific to the network;
 const Web3 = require('web3'); // bringing in a constructor
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 const { bytecode, interface } = require('../compile');
 
 let accounts;
+const INITIAL_STRING = 'Hi There'
 
 beforeEach(async () => {
-    //get a list of all account
+    //get a list of all local test accounts from ganache
     accounts = await web3.eth.getAccounts();
-
-    inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ['Hi there'] })
-        .send({ from: accounts[0], gas: '1000000'})
     //use one of them to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: [INITIAL_STRING] })
+        .send({ from: accounts[0], gas: '1000000'})
+    
+    //provider is passed into the web3 instance and set on the contract instance itself    
+    inbox.setProvider(provider) //remember providers are sort of the specifice key for web3 to interact with a given network
 })
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(inbox);
+        assert.ok(inbox.options.address); //check to see that an address was assigned meaning it was deployed properly
+    })
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call(); //console.log shows message returns an object with various methods including call
+        assert.strictEqual(message, INITIAL_STRING);
     })
 })
